fa7a36af650265329abe332df4f6a9062d32fe65
wubin666 <3239987006@qq.com>
2019-03-20 19:47:57 +0800

    v0.3 ok

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,40 @@
 module.exports = function (grunt){
   grunt.initConfig({
-  eslint: {
-    options: {
-      configFile: '.eslintrc.json'
+    eslint: {
+      options: {
+        configFile: '.eslintrc.json'
+      },
+      target: ['*.js']
     },
-    target: ['*.js']
-  },
-  csslint: {
-    options: {
-      csslintrc: '.csslintrc'
+    csslint: {
+      options: {
+        csslintrc: '.csslintrc'
+      },
+      src: '*.css'
     },
-    src: '*.css'
-  },
-  htmlhint: {
-    options: {
-       htmlhintrc: '.htmlhintrc'
+    htmlhint: {
+      options: {
+        htmlhintrc: '.htmlhintrc'
+      },
+      src: '*.html'
     },
-    src: '*.html'
-  }  
+    mocha: {
+      test: {
+        src: ['test/index.html'],            
+      },
+      options: {
+        run: true,
+        reporter: 'Spec'                      
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-htmlhint');
   grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-mocha');
+
   grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
+  grunt.registerTask('unTest',['mocha']);
 
 };
--- rectangle.js
+++ rectangle.js
@@ -5,14 +5,13 @@ $(function() {
       $perimeter = $('#perimeter'),
       $area = $('#area');
 
-$btnCal.click(function(){
-  var w = Number($width.val()),
-      h = Number($height.val());
+  $btnCal.click(function(){
+    var w = $width.val(),
+        h = $height.val();
 
-  var p = 2 * (w + h),
-      a = w * h;
-
-  $perimeter.val(p);
-  $area.val(a);       
+    var r=new Reactangle(w,h);
+    
+    $perimeter.val(r.perimeter());
+    $area.val(r.area());
   });
 });
